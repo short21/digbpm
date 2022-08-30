84affe65b7bf53ceb602da493787fc07d03e94f3
EC2 Default User <hluas.qus.dawb@gmail.com>
2022-08-30 14:14:40 +0000

    first commit

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,6 +1,6 @@
 class MusicsController < ApplicationController
   require 'rspotify'
-  RSpotify.authenticate(ENV['short'], ENV['short'])
+  RSpotify.authenticate(ENV['49bcca4918aa40a9b517faf7ebdf42c7'], ENV['9c88780e97d041dcbcf6e3a9763f98ba'])
 
   def search
     @musics = Music.all
