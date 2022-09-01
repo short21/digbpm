c129297fa615f711e3c68a00ca0525f3a7688db8
EC2 Default User <hluas.qus.dawb@gmail.com>
2022-09-01 15:48:27 +0000

    add

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,11 +1,11 @@
 class MusicsController < ApplicationController
   require 'rspotify'
-  RSpotify.authenticate(ENV['49bcca4918aa40a9b517faf7ebdf42c7'], ENV['9c88780e97d041dcbcf6e3a9763f98ba'])
+  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
 
   def search
     @musics = Music.all
     if params[:search].present?
-    @searchartists = RSpotify::Artist.search(params[:search])
+      @searchartists = RSpotify::Artist.search(params[:search])
     end
   end
 end
